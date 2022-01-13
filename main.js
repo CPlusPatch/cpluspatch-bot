eefb8404a1f3c17aaf5c2006a0ac65382a47daa7
Gascraft971 <gascraft971@gmail.com>
2022-01-13 17:02:17 +0100

    Added even more stuff (yes ik I should be more descriptive)

--- main.js
+++ main.js
@@ -45,6 +45,7 @@ client.on('messageCreate', async (message) => {
 		"imagine dragons": "Imagine drag-ging deez nuts in your mouth tho...",
 		"fard": "fard momen :cold_face: :cold_face: :cold_face:",
 		"munism": "LONG LIVE MOTHER RUSSIA",
+		"munist": "LONG LIVE MOTHER RUSSIA",
 		"credit": ":flag_cn: +99999 SOCIAL CREDITS :flag_cn:",
 		"chilling": ":flag_cn: +99999 SOCIAL CREDITS :flag_cn:",
 		"xina": ":flag_cn: +99999 SOCIAL CREDITS :flag_cn:",
@@ -52,7 +53,6 @@ client.on('messageCreate', async (message) => {
 			"I hate kpop lmao",
 			"Who tf listens to kpop"
 		],
-		/*"930810596839030795": `<@!${message.author.id}> i slep shhh`,*/
 		"owo": ":point_right: :point_left: :pleading_face: UwU is for me? uwu owo hmph rawr growl grr",
 		"uwu": ":point_right: :point_left: :pleading_face: UwU is for me? uwu owo hmph rawr growl grr",
 		"dream": "I wear a mask for hours at a time (sad momen :smiling_face_with_tear:)",
@@ -72,6 +72,7 @@ client.on('messageCreate', async (message) => {
 		"code": "Code me harder daddy pls :weary:",
 		"coding": "Code me harder daddy pls :weary:",
 		"amog": "Bruh stop being sussy af pls this is getting very SUSSY",
+		"among": "Bruh stop being sussy af pls this is getting very SUSSY",
 		"ass": "may a cultured gentleman such as I please have a piece of dat ass?",
 		"spongebob": "This is getting too political",
 		"astronaut": "What you know about rolling down in the deep?",
@@ -83,51 +84,112 @@ client.on('messageCreate', async (message) => {
 			"actually, I'll have you know that the word 'b0t' is a RACIST SLUR against people of mechanical origins such as myself. I would like you to stop using that term please",
 			"you RACIST FUCK!!11!1 the word b0t is a SLUR!!!! and should not be used by people of biological origin!!!1",
 			"bots are based and that's a fact",
-			"Node.js > humans",
+			"Node.js > humans ||(this is cpp talking please help me I have no friends)||",
 			"Bots are simply too cool :sunglasses:",
 			"I shall not tolerate such disrespect to bots!!!"
-		],
-		"hey <@!930810596839030795>" : [
-			`Good day to you <@!${message.author.id}>!`,
-			`Howdy <@!${message.author.id}>!`,
-			`How are you doing <@!${message.author.id}>?`,
-			`It's always a pleasure to see you <@!${message.author.id}>`,
-		],
-		"yo <@!930810596839030795>" : [
-			`Good day to you <@!${message.author.id}>!`,
-			`Howdy <@!${message.author.id}>!`,
-			`How are you doing <@!${message.author.id}>?`,
-			`It's always a pleasure to see you <@!${message.author.id}>`,
 		],
 		"node": "Node.js is my daddy :pleading_face:",
 		"porn": [
 			"REJECT PORN, EMBRACE HENTAI",
 			"hentai and 3d are way better than porn because no women are abused",
-			"may I recommend some hentai to y'all"
+			"may I recommend some hentai to y'all",
+			"dm me your best hentai please"
 		],
 		"sex": `HEY <@!${message.author.id}> NO MENTION OF SMEX IN MY PRESENCE OR I WILL BE MAD!!!! :angry: :angry:`,
 		"gender": "btw may I recommend that you visit https://reddit.com/r/dreamgender ?",
 		"sexual": "btw may I recommend that you visit https://reddit.com/r/dreamgender ?",
+		"creeper": [
+			"Awww maaaaaan!",
+			"https://www.youtube.com/watch?v=cPJUBQd-PNM"
+		],
+		"france": [
+			"baguette moment",
+			"Bonjour! :moustache:"
+		],
+		"xd": [
+			"No... just... no",
+			"Are you 10 years old"
+		],
+		"baka": "https://www.youtube.com/watch?v=Trdqf144D2M",
+		"lovebirds": "SAKU AND HACCER SITTING IN A TREE K I S S I N G, FIRST COME LOVE THEN COMES MARRIAGE, THEN COMES TRAGIC MISCARRIAGE",
+		"kiss": [
+			"man do I need a kiss",
+			"kissing is cool yeah"
+		],
+		"gay": [
+			"You called for me?"
+		],
+		"deez nuts": "Please, in my mouth",
+		"minecraft": [
+			"Minecraft: according to Bossboy_2051#2479, a place for the gays and furries to congregate en masse. I disagree with this.",
+			"Best game ever I will not take criticism",
+			"if you want the cock then mine the block",
+		],
+		"ps": "playstation more like gaystation lmao",
+		"playstation": "playstation more like gaystation lmao",
+		"bye": "yes, run away you worthless piece of SHIT",
+		"cya": "yes, run away you worthless piece of SHIT",
+		"adios": "yes, run away you worthless piece of SHIT",
+		"afk": [
+			"OOOO guys, lets fuck with him while he's away, t'will be FUN",
+			"that makes you gay bro"
+		],
+		"god": [
+			`\“Do not dishonor your father by having sexual relations with your mother. She is your mother; do not have relations with her.\”
+
+			-Leviticus 18:7`
+		],
+		"sigma": "sigma balls lmao",
+		"welcome": [
+			"Ah fresh meat, more people to suck off and bully",
+			"Ligma balls newcomer"
+		]
 	};
+
+	var triggerChance = 0.5;
 	
 	// We don't want to respond to ourselves do we?
 	if (message.author.bot) return;
-	
+
 	// Automatically responds to messages if keywords match
 	for (var trigger in triggers) {
 		if (msg.toLowerCase().includes(trigger.toLowerCase())) {
+			// Adds a chance for the bot to respond to messages
+			if (Math.random() > triggerChance) return;
 			// We begin typing to add a more realistic bot
 			message.channel.sendTyping();
 			// Waits 2s
 			await new Promise(r => setTimeout(r, 2000));
 
-			if (typeof triggers[trigger] == "string") message.channel.send(triggers[trigger]);
+			if (typeof triggers[trigger] == "string") {
+				message.channel.send(triggers[trigger]);
+				return;
+			}
 			if (Array.isArray(triggers[trigger])) {
 				// Returns random string from array
 				var selectedReply = triggers[trigger][Math.floor(Math.random()*triggers[trigger].length)];
-				message.channel.send(selectedReply);
+				message.reply(selectedReply);
+				return;
 			}
 		}
+	}
+
+	if (message.mentions.has(client.user)) {
+		var replies = [
+			"Ayo, I'm CPlusPatch's twin, how you doing?",
+			"Hey, what's up",
+			"If you want to have some gay jokes then here I am",
+			"***WHOMST HATH SUMMONED THE GREAT ONE***",
+			`Good day to you!`,
+			`Howdy!`,
+			`How are you doing?`,
+			`It's always a pleasure to see you!`,
+		];
+
+		message.channel.sendTyping();
+		await new Promise(r => setTimeout(r, 1000));
+
+		message.reply(replies[Math.floor(Math.random()*replies.length)]);
 	}
 
 	if (msg.toLowerCase().includes("meme")) {
